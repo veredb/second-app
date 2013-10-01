aae25b4dca51239ae152a9dfae18918942980930
Vered Bauer <veredbauer@Vereds-MacBook-Air.local>
2013-10-01 14:47:22 -0500

    commit before changes in database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130920013328) do
+ActiveRecord::Schema.define(version: 20130930172652) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -22,9 +22,8 @@ ActiveRecord::Schema.define(version: 20130920013328) do
     t.string   "textbox"
     t.string   "middle"
     t.string   "last"
-    t.string   "status"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
